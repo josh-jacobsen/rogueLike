a6188d5ac24d13c94fc9fcfa50ae197cda13e84c
Josh Jacobsen <joshjacobsen5@gmail.com>
2016-12-19 15:22:59 +1300

    set up project and tests

---
+++ RogueTests/ProgramTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xunit;
+
+namespace RogueTests
+{
+    public class ProgramTests
+    {
+        [Fact]
+        public void Working()
+        {
+            Assert.True(true);
+        }
+    }
+}
